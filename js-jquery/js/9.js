6397d1068f1012672cc356c7367d7f7c8b3983e0
Benoît |||| del <63121195+dellarabica@users.noreply.github.com>
2022-06-10 01:26:53 +0200

    finished e9

--- js-jquery/js/9.js
+++ js-jquery/js/9.js
@@ -1,96 +1,53 @@
-var rHexVal,
-    gHexVal,
-    bHexVal,
-    rRGBVal,
-    gRGBVal,
-    bRGBVal;
-
 function colorHexa(input) {
     var error = false,
-        saisie = input.value,
-        hexacode = "";
+        regex = /^[0-9A-F]{2}$/i;
 
     if (error) {
         error = false;
     } else {
-        if (saisie < "00" || saisie > "FF") {
+        if (!regex.test($(input).val())) {
             window.alert("Veuillez saisir une valeur hexadécimale valide comprise entre 00 et FF");
             input.focus();
             error = true;
             return false;
-        } else {
-            var name = input.name;
-            switch (name) {
-                case "Rouge":
-                    rHexVal = input.value;
-                    alert('Je suis dans la case rouge et rHexVal = ' + rHexVal);
-                    break;
-                case "Vert":
-                    gHexVal = input.value;
-                    alert('Je suis dans la case verte et gHexVal = ' + gHexVal);
-                    break;
-                case "Bleu":
-                    bHexVal = input.value;
-                    alert('Je suis dans la case bleue et gHexVal = ' + bHexVal);
-                    break;
-                default:
-                    window.alert('Aucune correspondance hexadécimale');
-            }
         }
     }
-    hexacode = "#" + rHexVal + gHexVal + gHexVal;
-    document.body.style.backgroundColor = hexacode;
+
+    $('body').css('background-color', '#' + $('#RougeHexa').val() + $('#VertHexa').val() + $('#BleuHexa').val());
 }
 
 function colorRGB(input) {
-    var error = false,
-        saisie = parseInt(input.value, 10),
-        rgbcode = "";
+    var error = false;
 
     if (error) {
         error = false;
     } else {
-        if (saisie < 0 || saisie > 255) {
+        if (!(0 <= parseInt($(input).val(), 10) <= 255)) {
             window.alert("Veuillez saisir une valeur comprise entre 0 et 255");
             input.focus();
             error = true;
             return false;
-        } else {
-            var name = input.name;
-            switch (name) {
-                case "Rouge":
-                    rRGBVal = parseInt(input.value, 10);
-                    break;
-                case "Vert":
-                    gRGBVal = parseInt(input.value, 10);
-                    break;
-                case "Bleu":
-                    bRGBVal = parseInt(input.value, 10);
-                    break;
-                default:
-                    window.alert('Aucune correspondance RGB');
-            }
         }
     }
-    rgbcode = 'rgb(' + rRGBVal + ',' + gRGBVal + ',' + bRGBVal + ')';
-    document.body.style.backgroundColor = rgbcode;
+    $('body').css('background-color', 'rgb(' + parseInt($('#RougeRGB').val(), 10) + ',' + parseInt($('#VertRGB').val(), 10) + ',' + parseInt($('#BleuRGB').val(), 10) + ')');
 }
 
-window.addEventListener('load', function() {
-    rHexVal = document.getElementById('RougeHexa').value;
-    gHexVal = document.getElementById('VertHexa').value;
-    bHexVal = document.getElementById('BleuHexa').value;
-    rRGBVal = document.getElementById('RougeRGB').value;
-    gRGBVal = document.getElementById('VertRGB').value;
-    bRGBVal = document.getElementById('BleuRGB').value;
+$('#RougeHexa, #VertHexa, #BleuHexa').on('change', function() {
+    colorHexa(this);
+})
 
-    document.getElementById('btnRouge').addEventListener('click', function() {
-        document.body.style.backgroundColor = "red"
-    });
-    document.getElementById('btnVert').addEventListener('click', function() {
-        document.body.style.backgroundColor = "green"
-    });
-    document.getElementById('btnBleu').addEventListener('click', function() {
-        document.body.style.backgroundColor = "blue"
-    });
-});
+$('#RougeRGB, #VertRGB, #BleuRGB').on('change', function() {
+    colorRGB(this);
+})
+
+$('#btnBleu').on('click', function() {
+    $('body').css('backgroundColor', 'blue');
+})
+
+$('#btnRouge').on('click', function() {
+    $('body').css('backgroundColor', 'red');
+})
+
+$('#btnVert').on('click', function() {
+    $('body').css('backgroundColor', 'green');
+})

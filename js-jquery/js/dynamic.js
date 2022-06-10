4a09006ecc8f71989ea2d0348de677f5b090cf09
Benoît |||| del <63121195+dellarabica@users.noreply.github.com>
2022-06-10 11:44:54 +0200

    finished form
    OMFG IT'S FINALLY OVER DNCDIINDD

--- js-jquery/js/dynamic.js
+++ js-jquery/js/dynamic.js
@@ -1,14 +1,14 @@
 var nbLine = 1,
-    maxNbLine = 15;
-
-var dbBook = new Array();
-dbBook[0] = new Book('bk1', 'Le grand livre de JavaScript', 17);
-dbBook[1] = new Book('bk2', 'HTML encore plus vite', 15.99);
-dbBook[2] = new Book('bk3', 'Le guide pour bien débuter en CSharp', 25);
-dbBook[3] = new Book('bk4', 'Angular pour les nuls', 32);
-dbBook[4] = new Book('bk5', 'Rendre une page web dynamique en 45 minutes', 28);
-dbBook[5] = new Book('bk6', 'Comment créer son site web', 39.99);
-dbBook[6] = new Book('bk7', 'PHP pour les nuls', 25);
+    maxNbLine = 15,
+    dbBook = new Array(
+        new Book('bk1', 'Le grand livre de JavaScript', 17),
+        new Book('bk2', 'HTML encore plus vite', 15.99),
+        new Book('bk3', 'Le guide pour bien débuter en CSharp', 25),
+        new Book('bk4', 'Angular pour les nuls', 32),
+        new Book('bk5', 'Rendre une page web dynamique en 45 minutes', 28),
+        new Book('bk6', 'Comment créer son site web', 39.99),
+        new Book('bk7', 'PHP pour les nuls', 25)
+    );
 
 function Book(ref, name, uPrice) {
     this.ref = ref;
@@ -17,17 +17,18 @@ function Book(ref, name, uPrice) {
 }
 
 function showInfo(idSel) {
-    let idChoix = $('#' + idSel).charAt(idSel.length - 1),
-        val = parseInt($('#' + idSel).val(), 10);
-
+    let idChoix = $(idSel).attr("id").substring(6, $(idSel).attr("id").length),
+        val = parseFloat($(idSel).val());
     if (val >= 0) {
         $('#qte' + idChoix).val('0');
         $('#ref' + idChoix).val(dbBook[val].ref);
         $('#uPrice' + idChoix).val(dbBook[val].uPrice);
+        $('#price' + idChoix).val('0');
     } else {
         $('#qte' + idChoix).val('');
         $('#ref' + idChoix).val('');
         $('#uPrice' + idChoix).val('');
+        $('#price' + idChoix).val('');
     }
 }
 
@@ -45,21 +46,16 @@ function newLine() {
             '<td><input type="number" id="price' + nbLine + '" readonly="readonly"/> </td>' +
             '<td><input type="button" value="-" id="bt' + nbLine + '" class="btn"></td></tr>').insertBefore('#lineTot');
 
-
-
         $('#choice' + nbLine).on('change', function() {
-            showInfo(this.id);
-            calcPrice($('#qte' + nbLine).val(), nbLine);
+            showInfo(this);
+            calcPrice($('#qte' + (nbLine - 1)).val(), $('#qte' + (nbLine - 1)).id);
         })
         $('#qte' + nbLine).on('blur', function() {
-            calcPrice($(this).val(), nbLine);
+            calcPrice($(this).val(), $(this).attr("id").substring(3, $(this).attr("id").length));
         })
         $('#bt' + nbLine).on('click', function() {
             delLine(this);
         })
-
-
-
         nbLine++;
     } else {
         alert("Vous avez dépassé le nombre maximal de lignes !");
@@ -68,34 +64,28 @@ function newLine() {
 
 function calcPrice(val, id) {
 
-    let prix = (parseInt(val) * parseInt(document.getElementById('uPrice' + id).value));
-    if (val === '' || isNaN(prix)) prix = 0;
-    document.getElementById('price' + id).value = prix;
-    let tot = 0;
+    $('#TotFin').val(parseFloat('0'));
+    if (val === '0' || isNaN(parseFloat(val) * parseFloat($('#uPrice' + id).val()))) {
+        $('#price' + id).val(parseFloat('0'));
+    } else {
+        $('#price' + id).val((parseFloat(val) * parseFloat($('#uPrice' + id).val())).toFixed(2));
+
+    }
     for (let j = 0; j <= nbLine; j++) {
-        if (document.getElementById('price' + j) !== null) {
-            var prixCalc = document.getElementById('price' + j).value;
-            if (prixCalc > 0) {
-                tot = parseInt(tot) + parseInt(prixCalc);
+        if ($('#price' + j) !== null) {
+            if (parseFloat($('#price' + j).val()) > 0) {
+                $('#TotFin').val((parseFloat($('#TotFin').val()) + parseFloat($('#price' + j).val())).toFixed(2))
             }
         }
     }
-    // Affichage du total
-    document.getElementById('TotFin').value = tot;
 }
 
 function delLine(btn) {
-    let xid;
-    if (nbLine < 10) {
-        xid = btn.id.slice(-1);
-    } else {
-        xid = btn.id.substr(2, btn.id.length);
-    }
-    if (!(document.getElementById('price' + xid).value === '')) {
-        document.getElementById('TotFin').value = parseInt(document.getElementById('TotFin').value) - parseInt(document.getElementById('price' + xid).value);
-    }
-    document.getElementById('tab').removeChild(document.getElementById('line' + xid));
 
+    if (!($('#price' + $(btn).attr('id').substring(2, $(btn).attr('id').length)).val() === '')) {
+        $('#TotFin').val((parseFloat($('#TotFin').val()) - parseFloat($('#price' + $(btn).attr('id').substring(2, $(btn).attr('id').length)).val())).toFixed(2))
+    }
+    $('#tab #line' + $(btn).attr('id').substring(2, $(btn).attr('id').length)).remove();
     maxNbLine += 1;
 }
 

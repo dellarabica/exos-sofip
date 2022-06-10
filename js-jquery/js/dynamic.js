20b677d95970da7dd5373368bfa9ea8f49960b3c
Benoît |||| del <63121195+dellarabica@users.noreply.github.com>
2022-06-10 02:21:29 +0200

    wip form
    AAAAAAAAAAAAAAAAAAAAA

--- js-jquery/js/dynamic.js
+++ js-jquery/js/dynamic.js
@@ -17,38 +17,49 @@ function Book(ref, name, uPrice) {
 }
 
 function showInfo(idSel) {
-    let idChoix = idSel.charAt(idSel.length - 1),
-        val = parseInt(document.getElementById(idSel).value);
+    let idChoix = $('#' + idSel).charAt(idSel.length - 1),
+        val = parseInt($('#' + idSel).val(), 10);
 
     if (val >= 0) {
-        document.getElementById('ref' + idChoix).value = dbBook[val].ref;
-        document.getElementById('uPrice' + idChoix).value = dbBook[val].uPrice;
+        $('#qte' + idChoix).val('0');
+        $('#ref' + idChoix).val(dbBook[val].ref);
+        $('#uPrice' + idChoix).val(dbBook[val].uPrice);
     } else {
-        document.getElementById('ref' + idChoix).value = '';
-        document.getElementById('uPrice' + idChoix).value = '';
+        $('#qte' + idChoix).val('');
+        $('#ref' + idChoix).val('');
+        $('#uPrice' + idChoix).val('');
     }
 }
 
 function newLine() {
     if (nbLine < maxNbLine) {
         let option = '';
-        let tr = document.createElement("tr");
-        tr.id = "line" + nbLine;
         for (let i = 0; i < dbBook.length; i++) {
             option = option + '<option value="' + i + '">' + dbBook[i].name + '</option> ';
         }
-        tr.innerHTML = '<td><select name="choix" id="choice' + nbLine + '" onchange="showInfo (this.id); calcPrice(document.getElementById(\'qte' + nbLine + '\').value,' + nbLine + ');"> ' +
-            '<option value="-1">Choisissez votre livre</option> ' +
-            option +
-            '</select> </td>' +
+        $('<tr id="line' + nbLine + '"><td><select name="choix" id="choice' + nbLine +
+            '"><option value="-1">Choisissez votre livre</option> ' + option + '</select> </td>' +
             '<td><input type="text" id="ref' + nbLine + '" readonly="readonly" /> </td>' +
-            '<td><input type="number" min="0" step="1"  id="qte' + nbLine + '"onblur="calcPrice(this.value,' + nbLine + ');"/> </td>' +
+            '<td><input type="number" min="0" step="1" id="qte' + nbLine + '"/> </td>' +
             '<td><input type="number" id="uPrice' + nbLine + '" readonly="readonly"/> </td>' +
             '<td><input type="number" id="price' + nbLine + '" readonly="readonly"/> </td>' +
-            '<td><input type="button" value="-" id="bt' + nbLine + '" class="btn" onclick="delLine(this);"></td>';
+            '<td><input type="button" value="-" id="bt' + nbLine + '" class="btn"></td></tr>').insertBefore('#lineTot');
 
-        document.getElementById('tab').appendChild(tr);
-        document.getElementById('tab').insertBefore(tr, document.getElementById('lineTot'));
+
+
+        $('#choice' + nbLine).on('change', function() {
+            showInfo(this.id);
+            calcPrice($('#qte' + nbLine).val(), nbLine);
+        })
+        $('#qte' + nbLine).on('blur', function() {
+            calcPrice($(this).val(), nbLine);
+        })
+        $('#bt' + nbLine).on('click', function() {
+            delLine(this);
+        })
+
+
+
         nbLine++;
     } else {
         alert("Vous avez dépassé le nombre maximal de lignes !");
@@ -87,3 +98,11 @@ function delLine(btn) {
 
     maxNbLine += 1;
 }
+
+$(window).on('load', function() {
+    newLine();
+})
+
+$('#addLine').on('click', function() {
+    newLine();
+})
